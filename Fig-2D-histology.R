ecf84d2c478a9517681dd4edf3454779569b78f6
stan-iakhno <38441633+stan-iakhno@users.noreply.github.com>
2019-10-28 11:39:27 +0100

    Update Fig-2D-histology.R

--- Fig-2D-histology.R
+++ Fig-2D-histology.R
@@ -17,26 +17,40 @@ ft$variable <- factor(ft$variable, levels=c("diagnose",
                                             "crypt_abscess",
                                             "intra-epithelial_lymphocytes",
                                             "epithelial_damage"))
+library(plyr)
+ft$variable<-revalue(ft$variable, c("diagnose"="DI", "LP_eosinophils"="LPE",
+                       "LP_lymphocytes_&_plasma cells"="LPL",
+                       "LP_macrophages"="LPM","LP_neutrophils"="LPN","crypt_abscess"= "CA",
+                       "intra-epithelial_lymphocytes"="IEL", "epithelial_damage"="ED" ))
+
+
+ft$value <- factor(ft$value, levels = c('none','half','one','two'))
+ft$value<-forcats::fct_rev(ft$value)
 # plot and flip the axises
 library(ggplot2)
-pl01<-ggplot(ft, aes(x=variable, y=Freq, fill=value))+
-  geom_bar(stat="identity")+
+pl01<-ggplot(ft, increasing =T,aes(x=variable, y=Freq, fill=value))+
+  geom_bar(stat = "identity", width = 0.8)+
   facet_grid(.~diet)+
   coord_flip()+
-  labs(y = "Frequency", x="Histology analysis")+
+  labs( x="Histology analysis")+
   theme_minimal(base_size = 14,base_family = 'serif')+
- # theme(legend.title = element_text(hjust = 0.5, vjust = 0.5), legend.title.align = 0.)+
+#   theme(legend.title = element_text(hjust = 0), legend.title.align = 0.)+
   labs(fill="Histology score")+
   theme(axis.title.y = element_blank())+
-  theme(legend.direction = "horizontal",legend.position = c(0.25, 1.15))+
-  theme(legend.text = element_text(colour="black", size=10,vjust = 0.5),
-        legend.key.size = unit(2, 'mm'))
-
+theme(legend.direction = "horizontal",legend.position = c(0.5, -0.25))+
+ # theme(legend.text = element_text(colour="black", size=10, hjust = 0.8),
+ #       legend.key.size = unit(2, 'mm'))+
+  guides(fill = guide_legend(reverse=T))+
+  scale_fill_manual(values = c("#E69F00", "#009E73","#0090b2","#56B4E9"))+
+  theme(axis.title.x = element_text(colour="white"))+
+  theme(axis.text =  element_text(vjust = 0.5))+
+  theme(axis.text.x.bottom = element_text(vjust = 1))
 
 pl01 
-#save the plot on the HDD
-ggsave("hist001.png",device = "png", 
+
+ggsave("hist003.png",device = "png", 
        plot = pl01, 
        dpi = 300, 
-       width = 14, 
-       height = 7, units = 'cm')
+       width = 17.4, 
+       height = 6.28, units = 'cm')
+
