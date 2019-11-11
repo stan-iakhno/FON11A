6b29fe84ede43fa1fc6dd0044e92fc36f2d9e9b4
stan-iakhno <38441633+stan-iakhno@users.noreply.github.com>
2019-11-11 11:43:23 +0100

    Update Fig-S2C.R

--- Fig-S2C.R
+++ Fig-S2C.R
@@ -1,37 +1,34 @@
-
 # plots correaltion scatter-plot with the abline: colonic butyrate ~ liver 
-# CONTROLS
-#  Coefficients:
-#                       Estimate   SE       t-value  Pr(>|t|)    
-#  (Intercept)          2.130286   0.089043   23.92  9.36e-13 ***
-#  butyrate_CD$butyrate 0.045983   0.005872    7.83  1.76e-06 *** 
-#  Adjusted R-squared:  0.80 at 14 DF
-
+# YEAST
+#                       Estimate   SE       t-value Pr(>|t|)    
+# (Intercept)           2.17665    0.20681  10.525  4.94e-08 ***
+# butyrate_CD$butyrate  0.06294    0.01914   3.288  0.00539 ** 
+# Residual standard error: 0.2633 on 14 degrees of freedom
+# Adjusted R-squared:  0.3954 
 
 library(readxl)
+
 
 #fetches the data from an excel file
 butyrate_CD<-
   read_xlsx("C:/Users/stia/OneDrive - Norwegian University of Life Sciences/FOODSofNORWAY/FON_011/FON_011a/1_Manuscript/Caroline_fon11a/butyrate-CD.xlsx")
-
-#subset the controls
-butyrate_CD<-butyrate_CD[butyrate_CD$diet=="c",]
 
 # Fit regression line
 corr_eqn <- function(x,y, digits = 1) {
   corr_coef <- round(cor(x, y), digits = digits)
   paste("italic(r) == ", corr_coef)
 }
-labels = data.frame(x = 14, y = 3.25, label = corr_eqn(butyrate_CD$butyrate,
-                                                      butyrate_CD$liver))
 
+labels = data.frame(x = 14, y = 3.5, label = corr_eqn(butyrate_CD$butyrate,
+                                                    butyrate_CD$liver))
 
+#subset the yeast
+butyrate_CD<-butyrate_CD[butyrate_CD$diet=="y",]
 pl5<-ggplot(butyrate_CD, aes(x = butyrate, y = liver)) +
   geom_point(shape = 19, size = 4, aes(colour = diet)) +
-  scale_color_manual(values=c("#CC79A7",
-                              "#009E73"))+
+  scale_color_manual(values=c(  "#009E73"))+
   # scale_fill_manual(values=c("#CC79A7","#009E73"))
-  geom_smooth(colour = "#CC79A7", fill = "lightgreen", method = 'lm') +
+  geom_smooth(colour = "#009E73", fill = "lightgreen", method = 'lm') +
   #  ggtitle("Example") +
   ylab("liver index") +
   xlab("colonic butyrate, µM/g digesta") +
@@ -54,10 +51,9 @@ pl5<-ggplot(butyrate_CD, aes(x = butyrate, y = liver)) +
   theme(panel.grid = element_blank())
 pl5
 #save the plot on the HDD
-ggsave("cor03D-controls-but-liver.png",device = "png", 
+ggsave("cor0S3C-yeast-but-liver.png",device = "png", 
        plot = pl5, 
-       dpi = "retina", 
+       dpi = 300, 
        width = 8, 
-       height = 8, 
+       height = 8,
        units = "cm")
-
